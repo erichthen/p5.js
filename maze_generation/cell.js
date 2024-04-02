bc07bc9987226d7a281b75d2ec8dc30ae3db8f2d
erichthen <144511616+erichthen@users.noreply.github.com>
2024-04-02 00:56:41 -0700

    more progress on maze gen

---
+++ maze_generation/cell.js
@@ -0,0 +1,119 @@
+
+class Cell {
+
+    constructor(i, j) {
+
+       this.i = i;
+       this.j = j;
+       this.walls = [true, true, true, true];
+       this.visited = false;
+
+    }
+
+    static remove_wall(current, next) {
+
+        var x = current.i - next.i;
+
+        if (x === 1) {
+            current.walls[3] = false;
+            next.walls[1] = false;
+        }
+        else if (x === -1) {
+            current.walls[1] = false;
+            next.walls[3] = false;
+        }
+
+        var y = current.j - next.j;
+
+        if (y === 1) {
+            current.walls[0] = false;
+            next.walls[2] = false;
+        }
+        else if (y === -1) {
+            current.walls[2] = false;
+            next.walls[0] = false;
+        }
+
+    }
+
+    get_index(i, j) {
+
+        if (i < 0 || j < 0 || i > cols - 1 || j > rows - 1) {
+            return -1;
+        }
+        return i + j * cols;
+    }
+
+    check_neighbors() {
+
+        var neighbors = [];
+        var top = cells[this.get_index(this.i, this.j - 1)];
+        var bottom = cells[this.get_index(this.i, this.j + 1)];
+        var right = cells[this.get_index(this.i + 1, this.j)];
+        var left = cells[this.get_index(this.i - 1, this.j)];
+
+        if (top && !top.visited) {
+            neighbors.push(top);
+        }
+        if (bottom && !bottom.visited) {
+            neighbors.push(bottom);
+        }
+        if (right && !right.visited) {
+            neighbors.push(right);
+        }
+        if (left && !left.visited) {
+            neighbors.push(left);  
+        }
+        
+        if (neighbors.length > 0) {
+            var rand = floor(random(0, neighbors.length));
+            return neighbors[rand];
+        }
+        else {
+            return undefined;
+        }
+
+    }
+
+    highlight() {
+        
+        var x = this.i * size;
+        var y = this.j * size;
+        
+        noStroke();
+        fill(0, 250, 0);
+        rect(x, y, size, size);
+    }
+
+    show() {
+
+        var x = this.i * size;
+        var y = this.j * size;
+
+        stroke(255);
+
+        if (this.walls[0]){
+            line(x, y, x + size, y);
+        }
+
+        if (this.walls[1]){
+            line(x + size, y, x + size, y + size);
+        }
+
+        if (this.walls[2]){
+            line(x + size, y + size, x, y + size);
+        }
+
+        if (this.walls[3]){
+            line(x, y + size, x, y)
+        }
+
+        if (this.visited) {
+
+            noStroke();
+            fill(255, 0, 255, 100)
+            rect(x, y, size, size);
+            
+        }
+    }
+}
--- maze_generation/sketch.js
+++ maze_generation/sketch.js
@@ -1,31 +1,12 @@
 
 var cols, rows;
-var size = 40;
+var size = 20;
 var cells = [];
+var current;
 
-function Cell(i, j) {
-    
-    this.i= i;
-    this.j = j;
-
-    this.show = function() {
-
-        var x = this.i * size;
-        var y = this.j * size;
-
-        stroke(255);
-        noFill();  
-
-        rect(x, y, size, size);
-         
-        
-    }
-
-}
- 
 function setup() {
 
-    createCanvas(400, 400);
+    createCanvas(800, 800);
     cols = floor(width / size);
     rows = floor(height / size);
 
@@ -37,14 +18,29 @@ function setup() {
 
         }
     }
-
+    current = cells[0];
 }   
 
 
 function draw() {
 
     background(51);
+
     for (var i = 0; i < cells.length; i++){
         cells[i].show();
     }  
+
+    current.visited = true;
+    current.highlight();
+    var next = current.check_neighbors();
+    
+    if (next) {
+
+        next.visited = true;
+
+        Cell.remove_wall(current, next);
+
+        current = next;
+
+    }
 }
